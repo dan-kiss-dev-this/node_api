f6639762ea50f5007772cfc311d1773ec5d6f3e6
Dan Kiss <flowfast47@gmail.com>
2019-01-30 12:50:55 -0800

    add parameters of what data to accept

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,10 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
-    title: DataTypes.STRING
+    title: DataTypes.STRING,
+    allowNull: false,
   }, {});
   Todo.associate = function(models) {
     // associations can be defined here
+    Todo.hasMany(models.TodoItem, {
+      foreignKey: 'todoId',
+    });
   };
   return Todo;
 };
+
+//we modified todo.js to add the extra attribute allowNull: false
+//this attribute makes the title field not nullable so error is thrown if we try to add an empty or null value
+//Datatypes.string means we only expect a string value in the field
+
+//Also we created a relationship between Todo and TodoItem as every single Todo has a todoItem
+//that relationship is defined with the hasMany method of the Todo Model.
+//The foreignKey:'todoID' means that todoId is going to be the foreign key column in todoItem. You can read more about foreign keys here.
