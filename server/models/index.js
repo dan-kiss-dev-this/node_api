f5a814d69d62599594bf92539ed4627db8483ea5
Dan Kiss <flowfast47@gmail.com>
2019-01-29 16:12:54 -0800

    add in sequelize and postgres

---
+++ server/models/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+
+//__filename is a global variable in node, it is the current modules file's absolute path
+
+//consfit.use_env_variable checked if the enviornment variable is set, if it is set then we use the settings for that enviornment variable otherwise we use the alternative settings provided for that enviornment. the process.env gives access to the node env but this will only work with the dotenv package installed
