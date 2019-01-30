4777abdb01727c4e7bb74cb10438236dae48df6e
Dan Kiss <flowfast47@gmail.com>
2019-01-30 13:00:49 -0800

    add foreignkey:todoId to todoItem

--- server/migrations/20190130204018-create-todo-item.js
+++ server/migrations/20190130204018-create-todo-item.js
@@ -18,7 +18,16 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
-      }
+      },
+      todoId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Todos',
+          key: id,
+          as: 'todoId',
+        },
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
--- server/models/todoitem.js
+++ server/models/todoitem.js
@@ -1,10 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const TodoItem = sequelize.define('TodoItem', {
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
+    allowNull: false,
   }, {});
   TodoItem.associate = function(models) {
     // associations can be defined here
+    TodoItem.belongsTo(models.Todo, {
+      foreignKey: 'todoId',
+      onDelete: 'CASCADE',
+    })
   };
   return TodoItem;
 };
+
+//every single todo has many todoitems and each todoitem belongs to one todo
+//this is why we have the TodoItem.belongsTo defined in our model above.
+//the onDelete:'CASCADE' means if we delete a todo then the associated todoItem should also be deleted
+//
