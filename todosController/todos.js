2a3514fd797dd4a9414b309edd60a95a3ef6aec5
Dan Kiss <flowfast47@gmail.com>
2019-02-01 17:44:24 -0800

    update updateTodo controller

--- todosController/todos.js
+++ todosController/todos.js
@@ -70,46 +70,28 @@ class TodosController {
 
     updateTodo(req, res) {
         const id = parseInt(req.params.id, 10);
-        let todoFound;
-        let itemIndex;
-        db.map( (todo, index) => {
-            if (todo.id === id) {
-                todoFound = todo;
-                itemIndex = index;
+        models.Todo.update(
+            {
+                title: req.body.title,
+            },
+            {
+                where: {
+                    id,
+                }
             }
-        });
+        ).then(e => {
+            if (e) {
+                return res.status(201).send({
+                    success: 'true',
+                    message: 'todo updated successfully',
+                });
 
-        if (!todoFound) {
+            }
             return res.status(404).send({
                 success: 'false',
-                message: 'todo not found',
+                message: 'todo did not update',
             });
-        }
 
-        if (!req.body.title) {
-            return res.status(400).send({
-                success: 'false',
-                message: 'title is required'
-            });
-        } else if (!req.body.description) {
-            return res.status(400).send({
-                success: false,
-                message: 'description is required',
-            });
-        }
-
-        const newTodo = {
-            id: todoFound.id,
-            title: req.boby.title || todoFound.title,
-            description: req.body.description || todoFound.description,
-        }
-
-        db.splice(itemIndex,1,newTodo);
-
-        return res.status(201).send({
-            success: 'true',
-            message: 'todo added successfully',
-            newTodo,
         });
     }
 
